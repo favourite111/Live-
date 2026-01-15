75a3a1a695d5d9c5489f72823901ca6575bd90e3
favourite111 <eminentboy12345@gmail.com>
2026-01-15 17:04:27 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,11 @@
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:", err);
+});
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
